a17b6b699ef277a62ed7b8133bb2bb8fdf98d3b2
vishal6969 <vm925623@gmail.com>
2023-11-09 17:48:09 +0530

    add: splash screen and app icon

--- android/app/src/main/java/com/weatherapp/MainActivity.java
+++ android/app/src/main/java/com/weatherapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.weatherapp;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
@@ -15,6 +17,12 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "WeatherApp";
   }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme,true); 
+        super.onCreate(null);
+    }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
